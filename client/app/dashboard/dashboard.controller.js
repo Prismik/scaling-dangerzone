6ac7c6910be640335d75a60570281d7864a90377
Francis <francis.beauchampsc@gmail.com>
2014-10-29 12:41:15 -0400

    Several enhancements of the interface for the direction steps

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -5,18 +5,24 @@ var cities = [
     {
         city : 'Toronto',
         desc : 'This is the best city in the world!',
+        article: 'Pizza',
+        et: 'Half an hour',
         lat : 43.7000,
         long : -79.4000
     },
     {
         city : 'New York',
         desc : 'This city is aiiiiite!',
+        article: 'Magic carpet',
+        et: '1 hour',
         lat : 40.6700,
         long : -73.9400
     },
     {
         city : 'Chicago',
         desc : 'This is the second best city in the world!',
+        article: 'Sun glasses',
+        et: '20 minutes',
         lat : 41.8819,
         long : -87.6278
     }
@@ -39,8 +45,9 @@ var potential = [
 
 angular.module('sigApp')
   .controller('DashboardCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  	var infoWindows = [];
     var mapOptions = {
+    		scrollwheel: false,
         zoom: 4,
         center: new google.maps.LatLng(40.0000, -98.0000),
         mapTypeId: google.maps.MapTypeId.TERRAIN
@@ -55,21 +62,19 @@ angular.module('sigApp')
     
     $scope.directionsDisplay.setMap($scope.map);
     $scope.directionsDisplay.setOptions( { suppressMarkers: true } );
-    var infoWindows = [];
     
     var calcRoute = function () {
 		  var start = cities[0].lat + "," + cities[0].long;
-		  var end = "toronto, ont";
+		  var end = "toronto, ont"; // TODO how to decide of the end location ?
 		  var waypts = [];
 		  for (var i = 0; i < cities.length; i++) {
-		      waypts.push({
-		          location:cities[i].lat + "," + cities[i].long,
-		          stopover:true});
+		      waypts.push({ location:cities[i].lat + "," + cities[i].long,
+		          stopover:true });
 		  }
+
 		  for (var i = 0; i < potential.length; i++) {
-		      waypts.push({
-		          location:potential[i].lat + "," + potential[i].long,
-		          stopover:true});
+		      waypts.push({ location:potential[i].lat + "," + potential[i].long,
+		          stopover:true });
 		  }
 
 		  var request = {
@@ -80,58 +85,66 @@ angular.module('sigApp')
 		      travelMode: google.maps.TravelMode.DRIVING
 		  };
 
+		  // Call the direction service and on OK status, print the travel steps
 		  $scope.directionsService.route(request, function(response, status) {
 		    if (status == google.maps.DirectionsStatus.OK) {
 		      $scope.directionsDisplay.setDirections(response);
 		      var route = response.routes[0];
 		      var summaryPanel = document.getElementById('route');
-		      summaryPanel.innerHTML = '';
+		      var summary = '<h2>Direction steps</h2><br><table class="table">';
+		      var step = 1;
 		      // For each route, display summary information.
 		      for (var i = 0; i < route.legs.length; i++) {
-		        var routeSegment = i + 1;
-		        summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment + '</b><br>';
-		        summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
-		        summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
-		        summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+		      	var myRoute = route.legs[i];
+		      	console.log(myRoute);
+					  for (var j = 0; j < myRoute.steps.length; j++) {
+				     
+			        summary += '<tr>' + 
+			        	'<td>'+step.toString()+'</td>' +
+			        	'<td>'+myRoute.steps[j].instructions+'</td>' +
+			        	'<td>'+myRoute.steps[j].distance.value+' m</td>' +
+			        '</tr>';
+			        ++step;
+					  }
 		      }
+
+		      summary += '</table>'
+		      summaryPanel.innerHTML = summary;
 		    }
 		  });
 		}
 
    	var RouteControl = function(controlDiv) {
-
 		  // Set CSS styles for the DIV containing the control
 		  // Setting padding to 5 px will offset the control
 		  // from the edge of the map.
 		  controlDiv.style.padding = '5px';
 
 		  // Set CSS for the control border.
-		  var controlUI = document.createElement('div');
-		  controlUI.style.backgroundColor = 'white';
-		  controlUI.style.borderStyle = 'solid';
-		  controlUI.style.borderWidth = '2px';
-		  controlUI.style.cursor = 'pointer';
-		  controlUI.style.textAlign = 'center';
-		  controlUI.title = 'Click to set the map to Home';
-		  controlDiv.appendChild(controlUI);
+		  var ui = document.createElement('div');
+		  ui.style.backgroundColor = 'white';
+		  ui.style.borderStyle = 'solid';
+		  ui.style.borderWidth = '2px';
+		  ui.style.cursor = 'pointer';
+		  ui.style.textAlign = 'center';
+		  ui.title = 'Click to set the map to Home';
+		  controlDiv.appendChild(ui);
 
 		  // Set CSS for the control interior.
-		  var controlText = document.createElement('div');
-		  controlText.style.fontFamily = 'Arial,sans-serif';
-		  controlText.style.fontSize = '12px';
-		  controlText.style.paddingLeft = '4px';
-		  controlText.style.paddingRight = '4px';
-		  controlText.innerHTML = '<strong>Calculate route</strong>';
-		  controlUI.appendChild(controlText);
+		  var text = document.createElement('div');
+		  text.style.fontFamily = 'Arial,sans-serif';
+		  text.style.fontSize = '12px';
+		  text.style.paddingLeft = '4px';
+		  text.style.paddingRight = '4px';
+		  text.innerHTML = '<strong>Calculate route</strong>';
+		  ui.appendChild(text);
 
-		  // Setup the click event listeners: simply set the map to Chicago.
-		  google.maps.event.addDomListener(controlUI, 'click', function() {
+		  google.maps.event.addDomListener(ui, 'click', function() {
 		  	calcRoute();
 		  });
 		}
 
-		// Create the DIV to hold the control and call the HomeControl() constructor
-	  // passing in this DIV.
+		// Create a DIV and pass it the Route Calculation Control
 	  var routeControlDiv = document.createElement('div');
 	  var routeControl = new RouteControl(routeControlDiv);
 
@@ -144,24 +157,28 @@ angular.module('sigApp')
 	        '<div class="col-md-6">' +
 					  '<div class="form-group">' +
 					    '<label for="title">Title</label>' +
-					    '<input type="text" class="form-control" id="title" placeholder="Enter title...">' +
+					    '<input type="text" class="form-control" id="title" ' +
+					    			 'placeholder="Enter title..." value="' + info.city + '">' +
 					  '</div>' +
 					  '<select class="form-control">...</select>' +
 					  '<div class="form-group">' +
 					    '<label for="desc">Description</label>' +
 					    '<br>' +
 					    '<textarea class="form-control" style="resize: none;" id="desc" rows="5" cols="25">' +
+					    info.desc +
 					    '</textarea>' +
 					 	'</div>' +
 					'</div>' +
 					'<div class="col-md-6">' +
 						'<div class="form-group">' +
 					    '<label for="article">Article</label>' +
-					    '<input type="text" class="form-control" id="article" placeholder="Article...">' +
+					    '<input type="text" class="form-control" id="article" ' +
+					    			 'placeholder="Article..." value="' + info.article + '">' +
 					  '</div>' +
 					  '<div class="form-group">' +
 					    '<label for="estimate">Estimated time</label>' +
-					    '<input type="text" class="form-control" id="estimate" placeholder="Enter estimate...">' +
+					    '<input type="text" class="form-control" id="estimate" ' +
+					    			 'placeholder="Enter estimate..." value="' + info.et + '">' +
 					  '</div>' +
 				  	'<button type="submit" class="btn btn-default">Submit</button>' +
 					'</div>' +
@@ -205,64 +222,49 @@ angular.module('sigApp')
     }
 
     var createBlueMarker = function(info) {
-        
         var marker = new google.maps.Marker({
         		icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
             map: $scope.map,
             position: new google.maps.LatLng(info.lat, info.long),
-            title: info.city
+            title: info.city,
+            content: '<div class="infoWindowContent">' + info.desc + '</div>'
         });
-        marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
-        /*
-        google.maps.event.addListener(marker, 'click', function(){
-            infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
-            infoWindow.open($scope.map, marker);
-        });
-        */
         $scope.blueMarkers.push(marker);
 
-        
-        var infowindow = new google.maps.InfoWindow({ content: feedClientInfo('data') });
-        infoWindows.push(infowindow);
+        var infoWindow = new google.maps.InfoWindow({ content: feedClientInfo(info) });
+        infoWindows.push(infoWindow);
         google.maps.event.addListener(marker, 'click', function() {
-        	for (var i = 0; i < infoWindows.length; i++){
+        	for (var i = 0; i < infoWindows.length; i++) {
 			      infoWindows[i].close();
 			    }
-			    infowindow.open($scope.map,marker);
+			    infoWindow.open($scope.map,marker);
 			  });
     }
     
     var createRedMarker = function(info) {
-        
         var marker = new google.maps.Marker({
         		icon: "http://maps.google.com/mapfiles/ms/icons/red-dot.png",
             map: $scope.map,
             position: new google.maps.LatLng(info.lat, info.long),
-            title: info.city
+            title: info.city,
+            content: '<div class="infoWindowContent">' + info.desc + '</div>'
         });
-        marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
-        /*
-        google.maps.event.addListener(marker, 'click', function(){
-            infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
-            infoWindow.open($scope.map, marker);
-        });
-        */
         $scope.redMarkers.push(marker);
-
         
-        var infowindow = new google.maps.InfoWindow({ content: feedPotentialInfo('data') });
-        infoWindows.push(infowindow);
+        var infoWindow = new google.maps.InfoWindow({ content: feedPotentialInfo(info) });
+        infoWindows.push(infoWindow);
         google.maps.event.addListener(marker, 'click', function() {
-        	for (var i = 0; i < infoWindows.length; i++){
+        	for (var i = 0; i < infoWindows.length; i++) {
 			      infoWindows[i].close();
 			    }
-			    infowindow.open($scope.map,marker);
+			    infoWindow.open($scope.map,marker);
 			  });
     }
 
     for (var i = 0; i < cities.length; i++){
         createBlueMarker(cities[i]);
     }
+
     for (var i = 0; i < potential.length; i++){
         createRedMarker(potential[i]);
     }
