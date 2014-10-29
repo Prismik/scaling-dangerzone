141716c2bf54eb7f988b7e725ccd34a9d258443e
Francis <francis.beauchampsc@gmail.com>
2014-10-28 20:31:04 -0400

    Adds basic route calculation

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -46,12 +46,98 @@ angular.module('sigApp')
         mapTypeId: google.maps.MapTypeId.TERRAIN
     }
 
+		$scope.directionsService = new google.maps.DirectionsService();
+    $scope.directionsDisplay = new google.maps.DirectionsRenderer();
     $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-    $scope.markers = [];
+    $scope.redMarkers = [];
+    $scope.blueMarkers = [];
     
+    $scope.directionsDisplay.setMap($scope.map);
+    $scope.directionsDisplay.setOptions( { suppressMarkers: true } );
     var infoWindow = new google.maps.InfoWindow();
     
+    var calcRoute = function () {
+		  var start = cities[0].lat + "," + cities[0].long;
+		  var end = "toronto, ont";
+		  var waypts = [];
+		  for (var i = 0; i < cities.length; i++) {
+		      waypts.push({
+		          location:cities[i].lat + "," + cities[i].long,
+		          stopover:true});
+		  }
+		  for (var i = 0; i < potential.length; i++) {
+		      waypts.push({
+		          location:potential[i].lat + "," + potential[i].long,
+		          stopover:true});
+		  }
+
+		  var request = {
+		      origin: start,
+		      destination: end,
+		      waypoints: waypts,
+		      optimizeWaypoints: true,
+		      travelMode: google.maps.TravelMode.DRIVING
+		  };
+
+		  $scope.directionsService.route(request, function(response, status) {
+		    if (status == google.maps.DirectionsStatus.OK) {
+		      $scope.directionsDisplay.setDirections(response);
+		      var route = response.routes[0];
+		      var summaryPanel = document.getElementById('directions_panel');
+		      summaryPanel.innerHTML = '';
+		      // For each route, display summary information.
+		      for (var i = 0; i < route.legs.length; i++) {
+		        var routeSegment = i + 1;
+		        summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment + '</b><br>';
+		        summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+		        summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+		        summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+		      }
+		    }
+		  });
+		}
+
+   	var RouteControl = function(controlDiv) {
+
+		  // Set CSS styles for the DIV containing the control
+		  // Setting padding to 5 px will offset the control
+		  // from the edge of the map.
+		  controlDiv.style.padding = '5px';
+
+		  // Set CSS for the control border.
+		  var controlUI = document.createElement('div');
+		  controlUI.style.backgroundColor = 'white';
+		  controlUI.style.borderStyle = 'solid';
+		  controlUI.style.borderWidth = '2px';
+		  controlUI.style.cursor = 'pointer';
+		  controlUI.style.textAlign = 'center';
+		  controlUI.title = 'Click to set the map to Home';
+		  controlDiv.appendChild(controlUI);
+
+		  // Set CSS for the control interior.
+		  var controlText = document.createElement('div');
+		  controlText.style.fontFamily = 'Arial,sans-serif';
+		  controlText.style.fontSize = '12px';
+		  controlText.style.paddingLeft = '4px';
+		  controlText.style.paddingRight = '4px';
+		  controlText.innerHTML = '<strong>Calculate route</strong>';
+		  controlUI.appendChild(controlText);
+
+		  // Setup the click event listeners: simply set the map to Chicago.
+		  google.maps.event.addDomListener(controlUI, 'click', function() {
+		  	calcRoute();
+		  });
+		}
+
+		// Create the DIV to hold the control and call the HomeControl() constructor
+	  // passing in this DIV.
+	  var routeControlDiv = document.createElement('div');
+	  var routeControl = new RouteControl(routeControlDiv);
+
+	  routeControlDiv.index = 1;
+	  $scope.map.controls[google.maps.ControlPosition.TOP_RIGHT].push(routeControlDiv);
+
     var feedClientInfo = function(info) {
     	var popup = '<form role="form" style="width: 400px">' +
         '<div class="row">' +
@@ -133,7 +219,7 @@ angular.module('sigApp')
             infoWindow.open($scope.map, marker);
         });
         */
-        $scope.markers.push(marker);
+        $scope.blueMarkers.push(marker);
 
         
         var infowindow = new google.maps.InfoWindow({ content: feedClientInfo('data') });
@@ -157,7 +243,7 @@ angular.module('sigApp')
             infoWindow.open($scope.map, marker);
         });
         */
-        $scope.markers.push(marker);
+        $scope.redMarkers.push(marker);
 
         
         var infowindow = new google.maps.InfoWindow({ content: feedPotentialInfo('data') });
